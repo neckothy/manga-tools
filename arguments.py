8b99c80be5a276841c73398c863a432669dd2aba
neck <>
2023-08-03 06:06:46 -0400

    improve --help

--- arguments.py
+++ arguments.py
@@ -4,97 +4,146 @@ import argparse
 def parse(config):
     parser = argparse.ArgumentParser()
 
-    # clean
     parser.add_argument(
-        "-d", "--denoise", help="denoise pages (waifu2x lvl 1)", action="store_true"
-    )
-    parser.add_argument(
-        "-di", "--denoise-ignore", help="pages that shouldn't be denoised"
-    )
-    parser.add_argument(
-        "-g", "--grayscale", help="grayscale pages", action="store_true"
-    )
-    parser.add_argument(
-        "-gi", "--grayscale-ignore", help="pages that shouldn't be grayscaled"
-    )
-    parser.add_argument(
-        "-l",
-        "--level",
-        help="level pages, expects 'level' value for imagemagick",
-    )
-    parser.add_argument("-li", "--level-ignore", help="pages that shouldn't be leveled")
-    parser.add_argument(
-        "-o", "--optimize", help="losslessly optimize pages", action="store_true"
-    )
-
-    # rename
-    # common special pages (comma-split pages that should be tagged as x)
-    # not implemented yet cuz I'm dumb & lazy
-    parser.add_argument("-aft", "--afterword", help="Afterword")
-    parser.add_argument("-cov", "--cover", help="Cover")
-    parser.add_argument("-ext", "--extra", help="Extra")
-    parser.add_argument("-toc", "--table-of-contents", help="ToC")
-    # normal stuff
-    parser.add_argument(
-        "-cn",
-        "--chapter-numbers",
-        help="comma-split chapter numbers, accepts ranges (1..5,5x1)",
-    )
-    parser.add_argument(
-        "-cp", "--chapter-pages", help="comma-split chapter starting pages"
-    )
-    parser.add_argument(
-        "-ct", "--chapter-titles", help="TWO comma-split chapter titles (title,,title2)"
-    )
-    parser.add_argument(
-        "-nr", "--no-rename", help="don't rename images", action="store_true"
-    )
-    parser.add_argument("-p", "--publisher", help="publisher name")
-    parser.add_argument("-r", "--ripper", help="ripper tag")
-    parser.add_argument("-t", "--title", help="series title")
-    parser.add_argument("-v", "--volume", help="volume number")
-    parser.add_argument("-y", "--year", help="published year")
-    parser.add_argument(
-        "-cpad",
-        "--chapter-padding",
-        help="zero padding of chapter number",
-        type=int,
-        default=3,
-    )
-    parser.add_argument(
-        "-vpad",
-        "--volume-padding",
-        help="zero padding of volume number",
-        type=int,
-        default=2,
-    )
-    parser.add_argument(
-        "-w", "--web", help="tag [web] instead of [dig]", action="store_true"
-    )
-
-    # archive
-    parser.add_argument(
-        "-z", "--zip", help="create a .cbz of finished imgs", action="store_true"
-    )
-    parser.add_argument(
-        "-ts", "--timestamp", help="modified timestamp, defaults to current time"
-    )
-
-    # join
-    parser.add_argument(
-        "-j",
-        "--join",
-        help="Join spreads - provide the page numbers of the right-hand page for each spread",
+        "-q", "--quiet", help="Disable console output of scripts", action="store_true"
     )
 
     # delete
     parser.add_argument(
         "-del",
         "--delete-pages",
-        help="comma-split list of pages to be removed (from the work folder)",
+        help="Comma-separated list of pages which should be removed",
     )
 
-    parser.add_argument("-q", "--quiet", help="shut up", action="store_true")
+    # clean
+    parser.add_argument(
+        "-o",
+        "--optimize",
+        help="Enable lossless optimization of pages",
+        action="store_true",
+    )
+    parser.add_argument(
+        "-d",
+        "--denoise",
+        help="Enable denoising of pages using DENOISE_LEVEL from config.py",
+        action="store_true",
+    )
+    parser.add_argument(
+        "-g", "--grayscale", help="Enable grayscaling of pages", action="store_true"
+    )
+    parser.add_argument(
+        "-l",
+        "--level",
+        help="Enable leveling of pages using provided value",
+    )
+    parser.add_argument(
+        "-di",
+        "--denoise-ignore",
+        help="Comma-separated list of pages which shouldn't be denoised",
+    )
+    parser.add_argument(
+        "-gi",
+        "--grayscale-ignore",
+        help="Comma-separated list of pages which shouldn't be grayscaled",
+    )
+    parser.add_argument(
+        "-li",
+        "--level-ignore",
+        help="Comma-separated list of pages which shouldn't be leveled",
+    )
+
+    # rename
+    parser.add_argument(
+        "-nr", "--no-rename", help="Disable renaming of pages", action="store_true"
+    )
+    parser.add_argument(
+        "-w",
+        "--web",
+        help="Use [web] tag on pages rather than [dig]",
+        action="store_true",
+    )
+    parser.add_argument(
+        "-cn",
+        "--chapter-numbers",
+        help="Comma-separated list of chapter numbers, accepts ranges (1..5,5x1)",
+    )
+    parser.add_argument(
+        "-cp", "--chapter-pages", help="Comma-separated list of chapter starting pages"
+    )
+    parser.add_argument(
+        "-ct",
+        "--chapter-titles",
+        help="TWO comma-separated list of chapter titles (title,,title2)",
+    )
+    parser.add_argument(
+        "-cov",
+        "--cover",
+        help="Comma separated list of pages which should be tagged [Cover]",
+    )
+    parser.add_argument(
+        "-toc",
+        "--table-of-contents",
+        help="Comma separated list of pages which should be tagged [ToC]",
+    )
+    parser.add_argument(
+        "-ext",
+        "--extra",
+        help="Comma separated list of pages which should be tagged [Extra]",
+    )
+    parser.add_argument(
+        "-aft",
+        "--afterword",
+        help="Comma separated list of pages which should be tagged [Afterword]",
+    )
+    parser.add_argument(
+        "-t", "--title", help="Series title to be used where appropriate"
+    )
+    parser.add_argument(
+        "-v", "--volume", help="Volume number to be used where appropriate"
+    )
+    parser.add_argument(
+        "-y", "--year", help="Publishing year to be used where appropriate"
+    )
+    parser.add_argument(
+        "-p", "--publisher", help="Publisher name to be used where appropriate"
+    )
+    parser.add_argument(
+        "-r", "--ripper", help="Ripper tag to be used where appropriate"
+    )
+    parser.add_argument(
+        "-cpad",
+        "--chapter-padding",
+        help="Zero-pad chapter number to specified length, default = 3",
+        type=int,
+        default=3,
+    )
+    parser.add_argument(
+        "-vpad",
+        "--volume-padding",
+        help="Zero-pad volume number to specified length, default = 2",
+        type=int,
+        default=2,
+    )
+
+    # archive
+    parser.add_argument(
+        "-z",
+        "--zip",
+        help="Create a zip (.cbz) archive of finished pages",
+        action="store_true",
+    )
+    parser.add_argument(
+        "-ts",
+        "--timestamp",
+        help="Set access/modify times for finished pages, default = current time",
+    )
+
+    # join
+    parser.add_argument(
+        "-j",
+        "--join",
+        help="Comma-separated list of spreads which should be joined, use only the lower page number (12,44)",
+    )
 
     args = parser.parse_args()
     args.config_pingo_path = config.PINGO_EXE_PATH
